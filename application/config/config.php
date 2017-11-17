f74c5d22aece6de11e26fd85a587e244ed4e0797
Ubuntu <ubuntu@ip-172-31-26-243.us-east-2.compute.internal>
2017-11-17 04:46:03 +0000

    11/17

--- application/config/config.php
+++ application/config/config.php
@@ -27,7 +27,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;port=3306;dbname=test;',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '129114l',
+			'password' => '123321',
 			'charset' => 'utf8mb4',
 			'tablePrefix' => 'lime_',
 		),
